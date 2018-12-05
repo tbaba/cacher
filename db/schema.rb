387b2376b085eabfa2399b6b10369a018fc9d353
Tatsuro Baba <harakirisoul@gmail.com>
2018-12-05 12:29:01 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_032353) do
+ActiveRecord::Schema.define(version: 2018_12_05_032810) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
